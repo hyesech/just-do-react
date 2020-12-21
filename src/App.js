ce90aa0188cbe489fec31880c790d4c21636a9f9
hyesech <hyesech@gmail.com>
2020-12-21 18:45:56 +0900

    css reset

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: #eeeeee;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 14px;
+  }
+`;
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div></div>
+    </>
+  );
 }
 
 export default App;
